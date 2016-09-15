9e5b989378823c3a9ba0392ba2df0740947ecbef
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-15 17:11:52 -0400

    Create modules for test suites. Elevate the access of get_message to public in order for the tests module to include it in its scope.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,40 @@
-#[test]
-#[should_panic]
-fn should_panic() {
-    assert!(false);
+pub fn add_two(a: i32) -> i32 {
+	a + 2
 }
 
-#[test]
-fn should_not_panic() {
-    assert!(true);
-}
+#[cfg(test)]
+mod tests {
+	use super::add_two;
 
-#[test]
-fn equality() {
-    assert_eq!("Hello", "Hello");
-}
+	fn should_add_two() {
+		assert_eq!(4, add_two(2));
+	}
 
-#[test]
-#[should_panic(expected = "assertion failed")]
-fn equality_should_panic() {
-    assert_eq!("Hello", "World");
-}
+    #[test]
+    #[should_panic]
+    fn should_panic() {
+        assert!(false);
+    }
 
-#[test]
-#[ignore]
-fn ignored_test() {
-    assert!(false);
+    #[test]
+    fn should_not_panic() {
+        assert!(true);
+    }
+    
+    #[test]
+    fn equality() {
+        assert_eq!("Hello", "Hello");
+    }
+    
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn equality_should_panic() {
+        assert_eq!("Hello", "World");
+    }
+    
+    #[test]
+    #[ignore]
+    fn ignored_test() {
+        assert!(false);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-fn get_hello_world_message() -> &'static str {
+
+
+pub fn get_hello_world_message() -> &'static str {
     "Hello, World!"
 }
 
@@ -6,7 +8,13 @@ fn main() {
     println!("{}", get_hello_world_message());
 }
 
-#[test]
-fn prints_hello_world() {
-    assert_eq!("Hello, World!", get_hello_world_message());
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+    #[test]
+    fn prints_hello_world() {
+        assert_eq!("Hello, World!", get_hello_world_message());
+    }
 }
